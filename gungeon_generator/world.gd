78c7b3e0d7d97144311c6ff2dbdd094e1fb5459d
Lukas Marusenko <63213730+Lukas311202@users.noreply.github.com>
2023-02-14 11:59:35 +0100

    Add files via upload

---
+++ gungeon_generator/world.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	LevelGenerator.border = $tilemap/border
+	LevelGenerator.ground = $tilemap/ground
+	LevelGenerator.watch_timer = $Timer
+	LevelGenerator.collision_detection = $tilemap/collision_detection
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
